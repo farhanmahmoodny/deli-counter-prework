fc2b588c9496c0fd5c6ebae32be722e264aa7405
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-13 03:44:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    new_line = []
+    counter = 0
+    until counter == katz_deli.length
+    new_line.push("#{counter + 1}. #{katz_deli[counter]}")
+    counter +=1
+  end
+puts "The line is currently: #{new_line.join(" ")}"
+end
+end
